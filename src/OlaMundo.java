d74c820bb3e7ad4610b78084941158591f96dc1c
gabrielmedina98 <gabriel_r1998@hotmail.com>
2022-03-02 20:25:11 -0300

    Caixa de texto

---
+++ src/OlaMundo.java
@@ -0,0 +1,12 @@
+
+import javax.swing.JOptionPane;
+
+
+public class OlaMundo {
+       public static void main (String args[]){
+
+      // System.out.println("ola mundo tudo certo por aqui");
+JOptionPane.showMessageDialog(null,"ola mundo tudo certo por aqui");
+
+  }
+}
